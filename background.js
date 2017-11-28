72ca6e31b572df769005865d4f8785596ff56355
mmitti <dev@mail.mmitti.info>
2017-11-29 01:43:03 +0900

    init commit

---
+++ background.js
@@ -0,0 +1,13 @@
+const theme = {
+    images: {
+        headerURL: 'dummy.png',
+    },
+    colors: {
+        accentcolor: '#004488',
+        textcolor: '#ffffff',
+        "toolbar": '#ffffff',
+        "toolbar_text": "#404040",
+        "toolbar_top_separator": "#0099ff",
+    }
+};
+browser.theme.update(theme);
